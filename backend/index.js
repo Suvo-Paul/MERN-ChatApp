c67430f7b85d933c9cf135400cc9a7537940522f
Suvo-Paul <suvopaul098@gmail.com>
2024-11-23 01:47:41 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+mongoose.connect(databaseURL).then(() => {
+    console.log("Database is connected");
+}).catch((err) => {
+    console.log(err.message);
+});
